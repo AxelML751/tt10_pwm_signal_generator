313135d4c9564954769bd7f4a1ea2919950b0473
AxelML751 <axelrml1125@gmail.com>
2025-03-06 03:28:08 -0600

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_pwm_signal_generator (
+  tt_um_pwm_signal_generator U0 (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
