1fde19c4f860315bfbc0d9cef5f31684e5c280cd
AxelML751 <axelrml1125@gmail.com>
2025-03-06 01:56:27 -0600

    Update tt_um_pwm_signal_generator.v

--- src/tt_um_pwm_signal_generator.v
+++ src/tt_um_pwm_signal_generator.v
@@ -32,9 +32,8 @@ module tt_um_pwm_signal_generator (
 	assign uo_out [7:1] = 7'0000000;  // Example: ou_out is the sum of ui_in and uio_in
 	assign uio_out [7:0] = 8'00000000;
 	assign uio_oe [7:0] = 8'00000000;
-	assign uio_in [7:0] = 8'00000000;
 
   // List all unused inputs to prevent warnings
-	wire _unused = &{ena, clk, rst_n, uo_ini[7:0], uio_oe[7:0], uio_out[7:0]};
+	wire _unused = &{ena, clk, rst_n, uio_in[7:0]};
 
 endmodule
